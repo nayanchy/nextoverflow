922ee208120b9cdeb5001f72fa22da7fe8dd346f
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-05-25 18:21:00 +0600

    Setup local fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="text-3xl">Welcome to NextJs BoilerPlate</div>;
+  return (
+    <div className="text-3xl">
+      Welcome to NextJs BoilerPlate
+      <h2 className="font-space-grotesk font-[700]">This is subtitle</h2>
+    </div>
+  );
 }
