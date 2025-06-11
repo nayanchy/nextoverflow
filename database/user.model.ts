480124d9a28b3d9f8a113f725426099dcf624402
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-06-11 16:18:47 +0600

    Connected Mongodb and created user model

---
+++ database/user.model.ts
@@ -0,0 +1,44 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  image: string;
+  bio?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    bio: { type: String },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
